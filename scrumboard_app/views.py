2bf878bbab49d0ec4ec03b461c9d55f65e920688
mattia <mattia5campus@gmail.com>
2019-07-19 15:58:45 +0200

    last templates html + css

--- scrumboard_app/views.py
+++ scrumboard_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
 from .forms import *
 from .models import *
 
